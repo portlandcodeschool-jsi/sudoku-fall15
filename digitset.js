7bdff6900fefb6f00c2a80f8b7a5d45267eed4f1
Jeff Dunn <jdunn522@gmail.com>
2015-08-27 09:17:35 -0700

    Add getBlock() method for the Grid instances.

--- digitset.js
+++ digitset.js
@@ -1,32 +1,32 @@
 var _ = require('lodash');
 
-
-
 function DigitSet(singleDigit) {
   this.possibilities = [];
 
+  if (singleDigit === ".") {
+     this.possibilities = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+   } else {
+     this.possibilities = [singleDigit];
+   }
+
   this.removePossibleValue = function(digit) {
     var location = this.possibilities.indexOf(digit);
-    this.possibilities.splice(location , 1);
+    this.possibilities.splice(location, 1);
   };
 
   this.size = function() {
     return this.possibilities.length;
   };
-  this.possibilitiesAsString = function(){
+
+  this.possibilitiesAsString = function() {
     return this.possibilities.join();
   };
-  this.possibilitiesAsArray = function(){
+
+  this.possibilitiesAsArray = function() {
     return this.possibilities;
   };
+};
 
-  if ( singleDigit === ".") {
-     this.possibilities = ["1","2","3","4","5","6","7","8","9"];
-   } else {
-     this.possibilities = [singleDigit];
-   }
-}
-
-//console.log(DigitSet);// for testing only
+// console.log(DigitSet); // for testing only
 
 module.exports = DigitSet;
--- grid.js
+++ grid.js
@@ -1,14 +1,15 @@
 var DigitSet = require('./digitset');
 
-
-function Grid(initstr) {
+function Grid(initStr) {
 	var self = this;
-	var strArray = initstr.split('');
+	var strArray = initStr.split("");
 	this.digitsets = [];
-	function initDigitSets( element , index , array ) {
+
+	function initDigitSets(element, index, array) {
   	var x = new DigitSet(element);
   	self.digitsets.push(x);
-	}
+	};
+
 	strArray.forEach(initDigitSets);
 
 	this.getRow = function(cellToken) {
@@ -19,8 +20,11 @@ function Grid(initstr) {
 		return cellToken%9;
 	};
 
-}
+	this.getBlock = function(cellToken) {
+		return 3*Math.floor(Math.floor(cellToken/9)/3)+Math.floor(cellToken%9/3);
+	};
+};
 
-console.log(Grid);// for testing only
+// console.log(Grid); // for testing only
 
 module.exports = Grid;
