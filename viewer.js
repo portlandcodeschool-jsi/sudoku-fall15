2091485faa6dd96100a9f5a269edd69d8e4e39bd
Pat <patharryux@gmail.com>
2015-08-27 11:35:41 -0700

    Add empty methods to Viewer.

--- viewer.js
+++ viewer.js
@@ -1,8 +1,39 @@
 // no need for grid module
 
 function SudokuViewer(grid) {
-	// your code here
-}
+
+	this.showCertain() {
+		//This will show a 9x9 grid of the Certain DigitSets
+	};
+
+	this.showPossible() {
+		//This will show a 9x9 grid of the DigitSets with all Possibilites
+	};
+
+	this.showDebug() {
+		//?
+	};
+
+	this.showHint(cellToken) {
+		//Show the Possibilities of a particular DigitSet
+	};
+
+	this.snapshot() {
+		//This will store a snapshot
+		//It will call grid.toString() , and store that value
+	};
+
+	this.playback() {
+		//This will replay all of the snapshots
+	};
+
+	this.startTimer() {
+		//?
+	};
+
+};
+
+
 SudokuViewer.prototype.show = function() {
 	console.log('Sudoku appears here!');
 }
