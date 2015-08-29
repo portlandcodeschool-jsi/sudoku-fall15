e4d8d71ae6da86b78109dc97b4dce413deeb971d
Dan Bauer <dsbauer@gmail.com>
2015-08-28 22:32:26 -0700

    Improved solver

--- solver.js
+++ solver.js
@@ -1,8 +1,8 @@
 /* Solver instance methods:
 
-solver.trim()		// try trimming each cell's possibilities
-solver.complete()	// try completing each group
-solver.guess()		// try guessing a cell and then examine the implications
+solver.trim()	// try trimming each cell's possibilities
+solver.grab()	// try grabbing digits needed to complete each group
+solver.guess()	// try guessing a cell and then examine the implications
 
 solver.solve() // try everything until finished
 
@@ -18,9 +18,23 @@ if (typeof _ === 'undefined')
 
 
 var Solver = (function() {
+
+	function Impossibility(str) {
+		this.str = str;
+	}
+
 	var score = 0,
 		total = 0,
-		debug = true;
+		debug = {
+			messages: true,
+			// when to draw board:
+			beforeGuess: false,
+			afterUndo: false,
+			afterEachTrim: false,
+			afterEachGrab: false,
+			afterTrimRound: false,
+			afterGrabRound: false,
+		};
 
 	function Solver(game) {
 		this.game = game;
@@ -29,22 +43,24 @@ var Solver = (function() {
 	}
 
 	Solver.prototype.report = function() {
-		if (debug) {
-			var spaces = '              ';
-			var indent = spaces.slice(0,this.gameState.length);
+		if (debug.messages) {
+			var spaces = '                      ';
+			var indent = spaces.slice(0,2*this.gameState.length);
 			arguments[0] = indent+arguments[0];
-			//console.log(arguments);
 			console.log.apply(console,arguments);
 		}
 	}
 	Solver.prototype.undo = function() {
-		var snap = this.restore();
-		var cell = snap.cell;
-		var guess = snap.guess;
-		this.report('Impossible for %s to be %s',this.game.cellName(cell),guess);
-		this.game.getPossible(cell).eliminate(guess);
-		//this.game.eliminate(cell,Sudoku.possible(guess));
+		var snapshot = this.restore();
+		var cell = snapshot.cell;
+		var guess = snapshot.guess;
+		this.report('Impossible for cell %s to be %s',this.game.cellName(cell),guess);
+		var newSet = this.game.getPossible(cell).eliminate(guess)
+		this.examineTrim(cell,newSet);
+		if (debug.afterUndo)
+			this.show();
 	}
+
 	Solver.prototype.store = function(cell,guess) {
 		this.gameState.push({
 			cell:cell,
@@ -55,14 +71,17 @@ var Solver = (function() {
 	Solver.prototype.restore = function() {
 		var snapshot = this.gameState.pop();
 		if (!snapshot) //uh-oh
-			throw "Snapshot stack is empty!"
+			throw new Impossibility("Found impossibility but can't backtrack!"); //"Snapshot stack is empty!"
 		//console.log(snapshot.data);
 		this.game.restore(snapshot.data);
 		return snapshot;
 	}
 	Solver.prototype.guess = function() {
-		if (this.gameState.length>15) throw ("Uh-oh, stack is too deep");
+		if (debug.beforeGuess)
+				this.show();
+		//if (this.gameState.length>15) throw ("Uh-oh, stack is getting deep");
 		var game = this.game;
+
 		var unsolved = game.cells().filter(function(cell) {
 			return game.getPossible(cell).size()>1;
 		});
@@ -73,20 +92,11 @@ var Solver = (function() {
 		var cell = unsolved[0];
 		var digits = game.getPossible(cell).toString();
 		var guess = digits[0];
-		this.report('Supposing that cell %s is %s...',game.cellName(cell),guess);
 		this.store(cell,guess);
+
+		this.report('Suppose that cell %s is %s...',game.cellName(cell),guess);
 		game.setPossible(cell,new DigitSet(guess));
 		return guess;
-	}
-	Solver.prototype.trim = function() {
-		var solver = this;
-		solver.game.cells().forEach(function(cell) {
-			var digit = solver.trimCell(cell);
-			if (digit)
-				solver.report('Cell %s must be %s',solver.game.cellName(cell),digit);
-		})
-		if (debug)
-			solver.viewer.showCertain();
 	}
 
 	// If the grid object has no neighborhood method, use this substitute:
@@ -96,15 +106,23 @@ var Solver = (function() {
 		var cells = groups.map(function(grp){return grid.cells(grp)});
 		cells=_.flatten(cells);
 		cells=_.uniq(cells);
-		//return cells;
 		var digits = cells.map(function(cell){return grid.getPossible(cell)})
 						.filter(function(digitset){return digitset.size()===1});
-		//return digits;
 		return digits.reduce(function(set1,set2) {return set1.add(set2)}, new DigitSet(0));
 	}
 
-	Solver.prototype.trimCell = function(cell) {//-->1-9 if newly unique, else undef
-	//!bug!  to fix: first build union of all neighbors, then eliminate from cell
+	Solver.prototype.examineTrim = function(cell,digitset) {
+		var size = digitset.size();
+		if (size===0)
+			throw new Impossibility('Cell '+ this.game.cellName(cell) + " can't be anything!");
+		if (size===1) {
+			this.report('Cell %s must be %s',this.game.cellName(cell),digitset.toString());
+			if (debug.afterEachTrim)
+				this.show();
+		}
+	}
+
+	Solver.prototype.trimCell = function(cell) {
 		if (this.game.getPossible(cell).size() ===1)
 			return;
 		var union = (this.game.neighborhood)?
@@ -112,58 +130,67 @@ var Solver = (function() {
 			this.neighborhood(cell);
 		var newSet = this.game.getPossible(cell).eliminate(union);
 		this.game.setPossible(cell,newSet);
-		if (newSet.size()===1)
-			return newSet.toString();
+		this.examineTrim(cell,newSet);
 	}
 
-	Solver.prototype.complete = function() {
+
+	Solver.prototype.grabGroup = function(group) {
 		var solver = this;
-		solver.game.groups().forEach(function(grp) {
-			solver.completeGroup(grp);
-		})
-		//if (debug)
-		//	solver.viewer.showCertain();
-	}
-
-	Solver.prototype.completeGroup = function(group) {
 		var game = this.game;
 		var digitsMissing = game.groupNeeds(group).toArray();
 		//console.log(digitsMissing);
 		digitsMissing.forEach(function(digit,bit) {
 			//console.log(digit,bit);
 			var possibleCells = game.cells(group).filter(function(cell) {
-				var digitset = game.getPossible(cell);
-				return digitset.contains(digit);
+				return game.getPossible(cell).contains(digit);
 			})
 			if (possibleCells.length===0) {
-				throw (game.groupName(group) + ' cannot get a '+digit+'!');
+				throw new Impossibility(game.groupName(group) + ' cannot get a '+digit+'!');
 			}
 			if (possibleCells.length===1) {
-				console.log(game.groupName(group) +' can only get '+digit+' from cell '+game.cellName(possibleCells[0]));
+				solver.report('%s can only get %s from cell %s',game.groupName(group),digit,game.cellName(possibleCells[0]));
 				game.setPossible(possibleCells[0],new DigitSet(digit))
+				if (debug.afterEachGrab)
+					solver.show();
 				//game.cellHas(possibleCells[0]);
 			}
 		})
+	}
+
+	Solver.prototype.grab = function() {
+		var solver = this;
+		solver.game.groups().forEach(function(grp) {
+			solver.grabGroup(grp);
+		})
+		if (debug.afterGrabRound)
+			solver.show();
+	}
+
+	Solver.prototype.trim = function() {
+		var solver = this;
+		solver.game.cells().forEach(function(cell) {
+			solver.trimCell(cell);
+		})
+		if (debug.afterTrimRound)
+			solver.show();
 	}
 
 	Solver.prototype.trimHard = function() {
 		var unsolved;
 		var lastProgress = Infinity;
 		var prevProgress = Infinity;
-		while (((unsolved=this.game.remaining()) < prevProgress) && unsolved) {
+		while ((unsolved=this.game.remaining()) && (unsolved < prevProgress)) {
 			prevProgress = lastProgress;
 			lastProgress = unsolved;
 			try {
 				this.trim();
-				this.complete();
-				if (debug) {
-					this.viewer.showCertain();
-					//console.log(this.game.remaining());
-				}
+				this.grab();
+				//console.log(this.game.remaining());
 			} catch (err) {
-				this.report(err);
-				//console.log(err);
-				this.undo();
+				if (err instanceof Impossibility) { //Found Sudoku impossibility; backtrack
+					this.report(err.str);
+					this.undo();
+				} else throw err; // some other error
 			}
 		}
 		return unsolved;		
@@ -173,16 +200,15 @@ var Solver = (function() {
 		var unsolved;
 		while (unsolved = this.trimHard()) {
 			// must've gotten stuck; time for a guess
-			if (debug)
-				this.viewer.showCertain();
 			this.guess();
 		}
-		if (!unsolved)
-			score++;
-		total++;
+		this.show();
 		return unsolved;
 	}
 
+	Solver.prototype.show = function() {
+		this.viewer.showCertain() // may want viewer.showPossible()
+	}
 
 	return Solver;
 })()
